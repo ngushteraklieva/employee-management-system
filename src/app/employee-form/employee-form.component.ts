a30560b7cad82def9139974dcd6532e63acadcde
ngushteraklieva <ngushteraklieva@gmail.com>
2026-01-08 13:20:41 +0100

    handle error messages

--- src/app/employee-form/employee-form.component.ts
+++ src/app/employee-form/employee-form.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css'
 })
 export class EmployeeFormComponent {
+  employee:Employee={
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    position: ''
+  }
 
+  errorMessage: string = ''
+  
+  constructor(private employeeService : EmployeeService, private router: Router){}
+
+  onSubmit(): void {
+    this.employeeService.createEmployee(this.employee)
+    .subscribe({
+      next: (response) =>{
+        this.router.navigate(['/'])
+      },
+      error: (err) => {
+        this.errorMessage = `Error occured (${err.status})`
+      }
+    })
+  }
 }
